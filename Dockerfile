3a42b1abee4f879f0e6cd62ce42b3bdaf8e48c27
Artem Nikulin <artem.nikulin@davita.com>
2020-03-29 22:19:57 +0300

    Fixed entity

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,8 @@ RUN rm -rvf build && rm -rvf k8s
 RUN mkdir build && cd build \
     && git clone https://github.com/lyoumi/k8s.git \
     && cd k8s \
-    && git log \
+    && git pull \
     && mvn clean install \
     && cd target \
-    && ls -la \
     && cp example-0.0.1-SNAPSHOT.jar /build/app.jar
 ENTRYPOINT exec java -jar /build/app.jar --debug
--- src/main/java/com/k8s/example/data/Users.java
+++ src/main/java/com/k8s/example/data/Users.java
@@ -1,5 +1,6 @@
 package com.k8s.example.data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.Data;
@@ -8,8 +9,12 @@ import lombok.Data;
 @Entity
 public class Users {
     @Id
+    @Column(name = "id")
     private Integer id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "age")
     private Integer age;
 }
