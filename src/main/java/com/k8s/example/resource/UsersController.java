74df7d263f519c10e1fd34ea36b5946ab897c8aa
Artem Nikulin <artem.nikulin@davita.com>
2020-03-29 20:26:45 +0300

    Added info logging

--- src/main/java/com/k8s/example/resource/UsersController.java
+++ src/main/java/com/k8s/example/resource/UsersController.java
@@ -4,6 +4,7 @@ import com.k8s.example.data.Users;
 import com.k8s.example.service.UserService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/users")
 @RestController
+@Slf4j
 public class UsersController {
 
     private final UserService userService;
 
     @GetMapping
     public List<Users> getAllUsers() {
+        log.info("Getting all users");
         return userService.getAllUsers();
     }
 
     @GetMapping(value = "/{id}")
     public Users getUserById(@PathVariable("id") Integer id) {
+        log.info("Getting user with id {}", id);
         return userService.getUserById(id);
     }
 }
